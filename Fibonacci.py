15c86623bedee39e9d9d78c87b414b04ce054e52
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-04 13:32:09 +0300

    Fibonacci
    Python dilinde;
    Girilen sayıya kadar tek tek fibonaci sayıları verir.

---
+++ Fibonacci.py
@@ -0,0 +1,10 @@
+sayi=int(input("Bir sayı giriniz: "))
+sayi1=1
+sayi2=1
+print(sayi1)
+print(sayi2)
+for i in range(sayi-2):
+    sayi3=sayi1+sayi2
+    sayi1=sayi2
+    sayi2=sayi3
+    print(sayi3)
