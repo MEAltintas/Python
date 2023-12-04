de129b0cf7dfd7b075bd582e7f83f52fb591c910
Münüre Ezgi <101413474+MEAltintas@users.noreply.github.com>
2023-12-04 13:35:43 +0300

    Delete Tahmin.py

--- Tahmin.py
+++
@@ -1,9 +0,0 @@
-sayi=int(input("Tahmin edilecek sayıyı giriniz: "))
-sayac=0
-for i in range(3):
-    tahmin=int(input("Tahmin ettiğiniz sayıyı giriniz: "))
-    if(sayi==tahmin):
-        print(i+1,". Tahminde bildiniz")
-        sayac=1
-if (sayac==0):
-    print("Doğru tahmin yapamadınız")
