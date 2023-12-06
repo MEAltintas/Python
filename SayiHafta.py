cbd7fd0b4d12dbaefe1dc7bb3a976d7ecb811068
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-06 23:32:53 +0300

    Sayıya göre gün
    Python dilinde;
    Kullanıcının girdiği sayıya göre haftanın gününü verir.

---
+++ SayiHafta.py
@@ -0,0 +1,15 @@
+sayi=int(input("Bir sayı giriniz: "))
+if(sayi==1):
+   print("Pazartesi")
+elif(sayi==2):
+    print("Salı")
+elif(sayi==3):
+    print("Çarşamba")
+elif(sayi==4):
+    print("Perşembe")
+elif(sayi==5):
+    print("Cuma")
+elif(sayi==6):
+    print("Cumartesi")
+else:
+    print("Pazar")
