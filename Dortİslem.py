012c17e4b584e7a57331475a19e86494d5e518ee
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-06 23:25:51 +0300

    Dört işlem
    Python dilinde;
    Girilen değerlerle dört işlem yapan fonksiyondur.

---
+++ Dortİslem.py
@@ -0,0 +1,11 @@
+def islem(a,b):
+    toplam=a+b
+    cikarma=a-b
+    carpma=a*b
+    bolme=a/b
+    return toplam,cikarma,carpma,bolme
+t,ci,ca,b=islem(8,4)
+print(t)
+print(ci)
+print(ca)
+print(b)
