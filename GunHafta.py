7592b0c69cd51398988971a2dfe46b693b30243b
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-06 23:31:02 +0300

    Güne göre haftanın kaçıncı günü
    Python dilinde;
    Kullanıcının girdiği güne göre haftanın kaçıncı günü olduğunu bulur.

---
+++ GunHafta.py
@@ -0,0 +1,15 @@
+gun=str(input("Gün giriniz: "))
+if(gun=="Pazartesi"):
+    print("Haftanın 1.günü")
+elif(gun=="Salı"):
+    print("Haftanın 2.günü")
+elif(gun=="Çarşamba"):
+    print("Haftanın 3.günü")
+elif(gun=="Perşembe"):
+    print("Haftanın 4.günü")
+elif(gun=="Cuma"):
+    print("Haftanın 5.günü")
+elif(gun=="Cumartesi"):
+    print("Haftanın 6.günü")
+else:
+    print("Haftanın 7.günü")
