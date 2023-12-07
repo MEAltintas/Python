6deb02f2303de69c0b719fa6fa597c7da7da9508
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-07 10:53:57 +0300

    Kare,küp,karekök hesaplama
    Python dilinde;
    0-11 arasında olan sayıların karesini, küpünü, karekökünü hesaplar.

---
+++ KareKup.py
@@ -0,0 +1,8 @@
+for i in range(0,11):
+    print("Sayı: ",i)
+    kare=float(i**2)
+    kup=float(i**3)
+    karekok=float(i**1/2)
+    print("Karesi: ",kare)
+    print("Küpü: ",kup)
+    print("Karekökü: ",karekok)
