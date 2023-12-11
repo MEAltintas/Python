aa1a55087f8730655076e56dd1af25523bc27d2e
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-11 22:05:37 +0300

    Liste üreteçleri
    Python dilinde;
    Bir listeyi dizi olarak tanımlamak yerine belirli şeylere atayarak da oluşturabiliriz. 5 sayı ekleme, koşul belirleme vs gibi.

---
+++ ListeUretecleri.py
@@ -0,0 +1,11 @@
+liste= [a for a in range(5)]
+print(liste)
+print()
+liste= [a*2+5 for a in range(5)]
+print(liste)
+print()
+liste = [a for a in range(11) if a%2 == 0]
+print(liste)
+print()
+liste = [a*2+5 for a in range(5) if (a<3)]
+print(liste)
