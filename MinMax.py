e7850649fce23d0d31c3e93824fa6b5e51bd2264
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-11 22:02:00 +0300

    Minimum maksimum değer bulma
    Python dilinde;
    Verilen listelerde en büyük sayı,en küçük sayı,kelime,hayvan,nesne vb. min-max değeri bulur.

---
+++ MinMax.py
@@ -0,0 +1,17 @@
+liste = [1, 2, 3, 4, 5, 6]
+print(min(liste))
+print(max(liste))
+
+print()
+
+print("Minimum değer=", min(liste), 'dür.')
+print(max(liste))
+print()
+kelime = ["10A", "10B", "9A"]
+print(min(kelime))
+print(max(kelime))
+print()
+kelime = ["Zürafa", "Aslan", "Fil"]
+print(min(kelime))
+print(max(kelime))
+print()
