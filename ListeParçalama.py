676e4ca074bb9137382fd8d7527feff6a753d2e8
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-11 21:47:03 +0300

    Liste parçalama
    Python dilinde;
    Verilen liste elemanlarını parçalayarak getirir.

---
+++ ListeParçalama.py
@@ -0,0 +1,7 @@
+liste4 = [2, 5, 7, 8, 3, 5, 8, 2]
+print(liste4[3:8:2])  # 3 ten başlar 8 e kadar 2 2 atlayarak gider. indis olduğu için 0 1 2 3 yani 3. indis 4.eleman oluyor.
+print(liste4[2:6])  # 2.indisten 6.indise kadar 6 dahil olmadan yazar
+print(liste4[4:])  # 4.indisten sonuna kadar yazar
+print(liste4[:4])  # 0.indisten yani başlangıçtan 4.indise kadar 4 dahil olmadan yazar
+print(liste4[::-1])  # başlangıçtan sona kadar -1 er yani (sondan başa gibi yazar(-1 olduğu için))
+print(liste4[::2]) 
