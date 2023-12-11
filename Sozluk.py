d195fdc66128b4fe3932d097600263948d23ac1c
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-11 22:09:11 +0300

    Sözlük yapısı
    Python dilinde;
    Süslü parantez içine girilen anahtarlar ve buna karşılık gelen değerlerdir.

---
+++ Sozluk.py
@@ -0,0 +1,14 @@
+
+# Sözlük
+sozluk = {}
+sozluk1 = {'a': 'aaa', 'b': 'bbb', 'c': 'ccc'}  # anahtarlar ve buna karşılık gelen değerler
+print(sozluk1)
+print(type(sozluk1))
+print()
+
+print('Merhaba', sozluk1["b"])
+print()
+
+sozluk1["e"] = "eee"
+sozluk1["b"] = "bb"
+print(sozluk1)
