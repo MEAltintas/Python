969e51c991777d67a2759d4af692c44d1ef0ccb6
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-12 17:16:12 +0300

    Numpy ile dizi oluşturma
    Python dilinde;
    Numpy ile oluşturulan dizinin tipini,boyutunu, kaça kaçlık olduğunu gösterir.

---
+++ NumpyDizi.py
@@ -0,0 +1,6 @@
+import numpy as np
+liste1=np.array([[4,5,6],[1,2,3],[7,8,9]],dtype=np.uint16)
+print(liste1)
+print(liste1.dtype) # Veri tipini gösterir "liste1" in
+print(liste1.ndim) # Boyut gösterir
+print(liste1.shape) #NxM'lik boyuttta
