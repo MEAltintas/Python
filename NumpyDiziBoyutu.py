34f0da546e09f78182a987644235c6783533abf1
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-12 17:17:53 +0300

    Numpy ile dizi boyutu
    Python dilinde;
    Numpy ile oluşturulan iki dizinin boyutunu,kaça kaçlık olduğunu gösterir.

---
+++ NumpyDiziBoyutu.py
@@ -0,0 +1,9 @@
+import numpy as np
+liste1=np.array([[1,2,3,4,5]])
+liste2=np.array([[1],[2],[3],[4],[5]])
+print(liste1)
+print(liste1.ndim) #2 boyutlu
+print(liste1.shape) #1x5 lik matris
+print(liste2)
+print(liste2.ndim) # 2 boyutlu
+print(liste2.shape) #5x1 lik matris
