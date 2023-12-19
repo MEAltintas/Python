e358c1e2457d39baf4ce18758bc2de8c2599e05e
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-19 17:03:57 +0300

    Pandas ile dataframe
    Python dilinde;
    Pandas ile dataframe oluşturma

--- PandasDataFrame.py
+++ PandasDataFrame.py
@@ -1,5 +1,10 @@
+import pandas as pd
 s1=pd.Series([2,4,6,8])
 s2=pd.Series(['c2','c4','c6','c8'])
 s3=dict(one=s1, two=s2)
 print(s3)
 print(type(s3))
+
+s4=pd.DataFrame(s3)
+print(s4)
+print(type(s4))
