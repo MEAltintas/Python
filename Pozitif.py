7a4a98a0eec03aae1bea6526dc6d50a03c4c326b
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-23 23:41:35 +0300

    Pozitif mi?
    Python dilinde;
    Kullanıcıdan girilen bir sayının pozitif olup olmadığını bulur.

---
+++ Pozitif.py
@@ -0,0 +1,7 @@
+# Kullanıcıdan bir sayı al
+sayi = float(input("Bir sayı girin: "))
+# Sayının pozitif olup olmadığını kontrol et
+if sayi > 0:
+    print("Girilen sayı pozitiftir.")
+else:
+    print("Girilen sayı pozitif değildir.")
