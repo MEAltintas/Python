b7f530df68a43bfa2d231d3507210a93737ce1e9
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-23 23:37:42 +0300

    Kinetik, Potansiyel,Mekanik enerji
    Python dilinde;
    Kinetik, potansiyel ve mekanik enerjiyi hesaplar.

---
+++ Mekanik.py
@@ -0,0 +1,10 @@
+m=int(input("m değeri giriniz: "))
+v=int(input("v değeri giriniz: "))
+h=int(input("h değeri giriniz: "))
+g=10
+kinetik=(m*(v**2))/2
+potansiyel=m*g*h
+mekanik=kinetik+potansiyel
+print("Kinetik Enerji: ",kinetik)
+print("Potansiyel Enerji: ",potansiyel)
+print("Mekanik Enerji: ",mekanik)
