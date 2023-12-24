26f08139f7eb506892cdb45461554dee261fdab5
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-24 17:32:56 +0300

    Formül hesabı
    Python dilinde;
    Kullanıcının girdiği x, y, a ve c değerleri ile formülün hesabını verir.

---
+++ Formul.py
@@ -0,0 +1,6 @@
+x=int(input("x değeri giriniz: "))
+y=int(input("y değeri giriniz: "))
+a=int(input("a değeri giriniz: "))
+c=int(input("c değeri giriniz: "))
+formul=(((x+y)*2)/c)+4*a
+print(formul)
