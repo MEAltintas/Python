4ad2c6fc885454110a2a9fbce13936ce82d6f7a4
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-26 00:12:10 +0300

    Diziler
    Python dilinde;
    sayilar adında boş bir diziye eleman ekleyip bunları listeleme

---
+++ Dizi2.py
@@ -0,0 +1,11 @@
+sayilar=[]
+sayilar.append(1)
+sayilar.append(2)
+sayilar.append(3)
+sayilar.append(4)
+sayilar.append(5)
+sayilar.append(6)
+sayilar.append(7)
+sayilar.append(8)
+for i in range(len(sayilar)):
+    print(sayilar[i])
