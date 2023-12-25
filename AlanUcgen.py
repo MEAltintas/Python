27355ce2fc8db95aaf03dd97da9555b3a6c044b7
Münüre Ezgi <mezgialtintas@gmail.com>
2023-12-25 19:26:16 +0300

    Üçgenin Alanı
    Python dilinde;
    Kullanıcıdan girilen kenar uzunluğu ve yüksekliğe göre alan hesaplar.

---
+++ AlanUcgen.py
@@ -0,0 +1,5 @@
+kenar = float(input("Lütfen üçgenin kenar uzunluğunu girin: "))
+yukseklik = float(input("Lütfen bu kenara ait yüksekliği girin: "))
+
+alan = (kenar * yukseklik) / 2
+print("Üçgenin Alanı: ",alan)
