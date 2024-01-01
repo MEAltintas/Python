e2653a3592e2345c7d007a9ac20683589a22a4d9
Münüre Ezgi <mezgialtintas@gmail.com>
2024-01-01 21:57:28 +0300

    Liste
    Python dilinde;
    Birbirine atanan ve copy işleminden sonra değişimleri gösteren örnektir.

---
+++ Liste5.py
@@ -0,0 +1,12 @@
+liste12=[1,2,6,4,5]
+print("Önce: ",liste12[2])
+liste13=liste12
+liste13[2]=3
+print("Sonra: ",liste12[2])
+
+
+liste14=[9,8,7,6]
+print("Önce: ",liste14[3])
+liste15=liste14.copy()
+liste15[3]=5
+print("Sonra: ",liste14[3])
