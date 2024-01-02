6fe0c98600fbd1d04418b782ac57f1dc0363a0cf
Münüre Ezgi <mezgialtintas@gmail.com>
2024-01-03 00:16:14 +0300

    if-elif-else
    Python dilinde;
    if-elif-else örneğidir.

---
+++ IfElse.py
@@ -0,0 +1,9 @@
+kahraman=input("Bir kahraman seçiniz: ")
+if(kahraman=="Batman"):
+    print("Ooo demek Batman hayranıyız!")
+elif(kahraman=="Örümcek Adam"):
+    print("Ağlarla savaş diyorsun demek :)")
+elif(kahraman=="Ironman"):
+    print("İşteee Demir Adam")
+else:
+    print("Kahramanınız yok mu?")
