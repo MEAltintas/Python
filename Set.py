0ec729cbcfb45c12879709ff121dc9e8bf748148
Münüre Ezgi <mezgialtintas@gmail.com>
2024-01-02 21:59:53 +0300

    Set
    Python dilinde;
    Boş bir set oluşturarak eleman ekleme işlemi yapılır.

---
+++ Set.py
@@ -0,0 +1,5 @@
+bosSetTanim=set()  # Boş bir set tanımlama
+bosSetTanim.add(10)
+bosSetTanim.add(20)
+bosSetTanim.add(10)
+print(bosSetTanim) # burada çıktı olarak sadece 10,20 verir diğer 10'u almamasının sebebi set işlemi fazlalığı almaz
